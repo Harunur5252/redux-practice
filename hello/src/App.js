e0e32b0999bdc268b44b1a6d758b3c80f1dd97c1
Harunur5252 <hroshid695@gmil.com>
2022-11-15 16:39:00 +0600

    initialize set up this project

---
+++ hello/src/App.js
@@ -0,0 +1,20 @@
+
+/*
+  Redux : Redux is global state management library.redux has a some flow.
+  1. store : from where you manage data. if a component need data then search store
+  2. reducer(function) : that manipulate all data   
+  Redux Toolkit : 
+*/
+
+function App() {
+  return (
+    <>
+      <p>10</p>
+      <button>Increment</button>
+      <button>Decrement</button>
+      <button>Reset</button>
+    </>
+  );
+}
+
+export default App;
